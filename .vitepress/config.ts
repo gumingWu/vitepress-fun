13ca0e27043d378683ac94896ff64c5a8a6bdf9e
gumingWu <15626185054@163.com>
2022-08-18 18:47:45 +0800

    首页样式优化

---
+++ .vitepress/config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'VitePress-Fun',
+  themeConfig: {
+    logo: '/cat.png',
+    siteTitle: 'VitePress-Fun',
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/gumingWu/vitepress-fun' }
+    ]
+  }
+})
