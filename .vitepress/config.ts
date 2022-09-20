b008e0c4914d8464eae2ab69c5f25455c167a0cc
gumingWu <15626185054@163.com>
2022-09-20 13:05:59 +0800

    先提交下

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,18 +1,32 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
+// --vp-code-block-bg
 export default defineConfig({
-  title: 'VitePress-Fun',
+  title: "VitePress-Fun",
+  markdown: {
+    theme: {
+      light: "vitesse-light",
+      dark: "vitesse-dark",
+    },
+  },
   themeConfig: {
-    logo: '/cat.png',
-    siteTitle: 'VitePress-Fun',
+    logo: "/cat.png",
+    siteTitle: "VitePress-Fun",
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/gumingWu/vitepress-fun' }
+      { icon: "github", link: "https://github.com/gumingWu/vitepress-fun" },
+    ],
+
+    sidebar: [
+      {
+        text: "基础",
+        items: [{ text: "快速开始", link: "/guide/" }],
+      },
     ],
 
     footer: {
-      message: '其实我也不知道写啥注脚',
-      copyright: '那我就随便写个吧',
-    }
-  }
-})
+      message: "其实我也不知道写啥注脚",
+      copyright: "那我就随便写个吧",
+    },
+  },
+});
---
+++ guide/index.md
@@ -0,0 +1,6 @@
+# Guide
+
+```js
+const str = '111'
+const num = 111
+```
