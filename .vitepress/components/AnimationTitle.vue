52b76f29f939d893f8c4ae6fdce1b3468277d8c8
gumingWu <15626185054@163.com>
2023-02-12 00:46:22 +0800

    实现动画标题效果

---
+++ .vitepress/components/AnimationTitle.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="title">Vitepress Fun Animation Title</div>
+</template>
+
+<style scoped>
+.title {
+  font-size: 48px;
+  line-height: 48px;
+  text-align: center;
+  margin-top: 20px;
+
+  background: -webkit-linear-gradient(
+    315deg,
+    rgb(210, 86, 53) 10%,
+    #647eff 50%,
+    rgb(238, 224, 112) 90%
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+  background-size: 400% 400%;
+  animation: gradient 5s ease infinite;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+
+  50% {
+    background-position: 100% 50%;
+  }
+
+  100% {
+    background-position: 0% 50%;
+  }
+}
+</style>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,12 +1,14 @@
-import Theme from 'vitepress/theme'
-import './style/var.css'
-import { h } from 'vue'
+import Theme from "vitepress/theme";
+import "./style/var.css";
+import { h } from "vue";
+import AnimationTitle from "../components/AnimationTitle.vue";
 
 export default {
   ...Theme,
   Layout() {
-    return h(Theme.Layout)
+    return h(Theme.Layout, null, {
+      "home-features-after": () => h(AnimationTitle),
+    });
   },
-  enhanceApp({ app }) {
-  }
-}
+  enhanceApp({ app }) {},
+};
